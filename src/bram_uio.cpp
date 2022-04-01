34483b61de25f0ce981c08283daa4c8ab200c3c6
Frederik Falk Nyboe <ffn@mmmi.sdu.dk>
2022-04-01 13:08:46 +0200

    Class functionality

---
+++ src/bram_uio.cpp
@@ -0,0 +1,25 @@
+/***************************** Include Files *********************************/
+#include "bram_uio.h"
+
+/************************** Function Implementation *************************/
+
+BRAM::BRAM(unsigned int uio_number, unsigned int size) {
+    char *device_file_name[20];
+    sprintf(device_file_name, "/dev/uio%d", uio_number);
+
+    int device_file;
+
+    if ((device_file = open(device_file_name, O_RDWR | O_SYNC)) < 0) {
+        std::stringstream ss;
+        ss << device_file_name << " could not be opened";
+        throw std::to_string(ss);
+    }
+
+    bram_ptr = (uint32_t *)mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, device_file, 0x0);
+
+    if (bram_ptr == NULL) {
+        std::stringstream ss;
+        ss << "Could not map memory";
+        throw std::to_string(ss);
+    }
+}
--- src/bram_uio.h
+++ src/bram_uio.h
@@ -2,12 +2,11 @@
 // Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2020.2 (64-bit)
 // Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
 // ==============================================================
-#ifndef BRAM_H
-#define BRAM_H
+#pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
 
 /***************************** Include Files *********************************/
 #include <stdint.h>
@@ -21,45 +20,18 @@ extern "C" {
 #include <unistd.h>
 #include <stddef.h>
 
-/**************************** Type Definitions ******************************/
+#include <string>
+#include <sstream>
 
-#define MAX_UIO_PATH_SIZE       256
-#define MAX_UIO_NAME_SIZE       64
+/******************************** Class ***********************************/
+class BRAM {
 
-typedef uint8_t u8;
-typedef uint16_t u16;
-typedef uint32_t u32;
-typedef uint64_t u64;
+public:
+    BRAM(unsigned int uio_number, unsigned int size);
 
-typedef struct {
-    u64 Control_BaseAddress;
-    u32 IsReady;
-} Xbram;
+    uint32_t& operator[](unsigned int);
 
-typedef u32 word_type;
+private:
+    uint32_t *bram_ptr;
 
-/***************** Macros (Inline Functions) Definitions *********************/
-#define Xbram_WriteReg(BaseAddress, RegOffset, Data) \
-    *(volatile u32*)((BaseAddress) + (RegOffset)) = (u32)(Data)
-#define Xbram_ReadReg(BaseAddress, RegOffset) \
-    *(volatile u32*)((BaseAddress) + (RegOffset))
-
-#define Xil_AssertVoid(expr)    assert(expr)
-#define Xil_AssertNonvoid(expr) assert(expr)
-
-#define XST_SUCCESS             0
-#define XST_DEVICE_NOT_FOUND    2
-#define XST_OPEN_DEVICE_FAILED  3
-#define XIL_COMPONENT_IS_READY  1
-
-/************************** Function Prototypes *****************************/
-int Xbram_Initialize(Xbram *InstancePtr, const char* device_file);
-int Xbram_Release(Xbram *InstancePtr);
-void Xbram_write(Xbram *InstacePtr, int offset, int length, uint32_t *data);
-void Xbram_read(Xbram *InstacePtr, int offset, int length, uint32_t *data);
-
-#ifdef __cplusplus
-}
-#endif
-
-#endif
+};
